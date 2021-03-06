f62713211c8045f58e46dc6c706d41b52b161b5b
Bryan Estrada <bryan@fanrock.com>
2021-03-06 12:15:59 -0800

    OAuth finally working

--- index.ts
+++ index.ts
@@ -18,10 +18,18 @@ app.get('/', (req,res) => res.json({"hello": "world"}));
 app.use('/form', new ApplicationRouter(auth).getRouter());
 app.use('/api', new ApiRouter(auth).getRouter());
 
+function errorNotification(err, str, req) {
+  console.log('ERROR', err);
+}
 // catch-all route
 app.use((req, res) => {
   console.error(`could not find path ${req.method} ${req.path}`);
   res.sendStatus(404);
+});
+
+app.use(function errorHandler(err, req, res, next) {
+  console.error(err);
+  res.sendStatus(500);
 });
 
 // package the the express app
--- routers/Authenticator.ts
+++ routers/Authenticator.ts
@@ -1,9 +1,12 @@
 import redis from "redis";
 import crypto from "crypto";
-import passport from "passport";
+import axios from "axios";
 import  session from "express-session";
+import querystring from "querystring";
+import _ from "underscore";
 
 import { Application, RequestHandler } from "express";
+import { access, readdirSync } from "fs";
 
 // old school import for redis type session
 const RedisStore = require("connect-redis")(session);
@@ -15,14 +18,17 @@ const REDIS_ENDPOINT:string = process.env.REDIS_ENDPOINT || '127.0.0.1:6379'
 const REDIS_PASSWORD:string = process.env.REDIS_PASSWORD || '';
 const SESSION_SECRET:string = process.env.SESSION_SECRET || '/r/savedyouaclick';
 
+
 const COOKIE_AGE_SECONDS:number = 30 * 60; // thirty minutes
 
 export default class Authenticator {
   private readonly _app:Application;
+  private readonly _host:string;
 
   public constructor(app:Application) {
     this._app = app;
     const isProd:boolean = ("prod" == process.env.NODE_ENV);
+    this._host = isProd ? "https://app.savedyouaclick.org" : "http://dev.savedyouaclick.org:3000";
 
     // need a connection to redis to hold sessions
     const redisClient = redis.createClient({
@@ -40,12 +46,14 @@ export default class Authenticator {
     this._app.use(
       session({
         genid: _req => this.createHashString(12),
+        
+        // cookie name changes is dev.
         name: isProd ? "sess_" : "dev_",
         cookie: {
           maxAge: COOKIE_AGE_SECONDS * 1000, // convert to miliseconds
-          domain: isProd ? ".savedyouaclick.org" : "dev.savedyouaclick.org",
+          domain: ".savedyouaclick.org",
           secure: isProd,
-          sameSite: isProd ? "none" : "strict"
+          sameSite: isProd ? "None" : "Lax"
         },
         proxy: isProd,
         resave: true,
@@ -61,15 +69,64 @@ export default class Authenticator {
           ttl : COOKIE_AGE_SECONDS + 60
         })
       })
-    );
-    this._app.use(passport.initialize());
-    this._app.use(passport.session());
+    );    
+
+    this._app.get('/auth/login', this.redirectToRedditOauth);
+    this._app.get('/auth/callback', this.redditAuthCallback);
   }
 
-  public reddit:RequestHandler = (req, res, next) => {
-    // TODO: ensure that the visitor is authenticated
-    // with reddit
-    next();
+  public reddit =  (req, res, next) => {
+    const reddit = req.session && req.session.reddit;
+    const now = new Date().getTime();
+    if (reddit && reddit.access_token && reddit.expires && reddit.expires > now) {
+      // todo: make sure the token is legit more explicitly
+      next();
+    } else {
+      res.sendStatus(401);
+    }
+  }
+
+  private redirectToRedditOauth:RequestHandler = (req, res) => {
+    const baseAuthUrl = "https://www.reddit.com/api/v1/authorize";
+    const params = {
+      client_id : process.env.REDDIT_CLIENT_ID,
+      response_type : "code",
+      state : this.createHashString(8),
+      redirect_uri : this._host + '/auth/callback',
+      scope: "identity submit"
+    };
+    res.redirect(`${baseAuthUrl}?${querystring.stringify(params)}`);
+  }
+
+  private redditAuthCallback = async (req, res) => {
+    const accessTokenUrl = "https://www.reddit.com/api/v1/access_token";
+    const formData = {
+      grant_type : "authorization_code",
+      redirect_uri : this._host + '/auth/callback',
+      code : req.query.code as string
+    };
+    const auth = {
+      username : process.env.REDDIT_CLIENT_ID as string,
+      password : process.env.REDDIT_CLIENT_SECRET as string
+    };
+    const headers = {
+      "Content-Type" : "application/x-www-form-urlencoded"
+    };
+    try {
+      const result = await axios.post(accessTokenUrl, querystring.stringify(formData), { auth, headers });
+      if (result && result.data && !result.data.error) {
+        console.log(result.data);
+        const reddit = _.omit(result.data, 'expires_in');
+        reddit.expires = result.data.expires_in + (new Date().getTime());
+        req.session.reddit = reddit;
+        res.redirect('/form');
+      } else {
+        res.status(500).json(result.data);
+      }
+    } catch (err) {
+      console.error(err);
+      res.sendStatus(500);
+    }
   }
 
   private createHashString = (howMany: number): string => {
