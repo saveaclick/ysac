308c6a17eb98ef7ac737206f814c8bfed3ff2e76
Lona.exe <39522664+JeswinSunsi@users.noreply.github.com>
2021-03-22 22:11:28 +0300

    Properly fixed icon paths

--- assets/theme.js
+++ assets/theme.js
@@ -4,11 +4,11 @@ theme = document.getElementById("themeID")
 
 theme.addEventListener("click", function(event) {
     if (main.classList.contains("light-mode")) {
-        theme.src = "night-icon.svg"
+        theme.src = "./assets/night-icon.svg"
         main.classList.remove("light-mode");
         event.preventDefault();
     } else {
-        theme.src = "day-icon.svg"
+        theme.src = "./assets/day-icon.svg"
         main.classList.add("light-mode");
         event.preventDefault();
     }
